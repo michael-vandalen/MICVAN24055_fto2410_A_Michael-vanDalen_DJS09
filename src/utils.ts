09124dbb94cba53075559cee253ccee55fa90f5b
Michael van Dalen <michaelvdalen191@gmail.com>
2025-04-13 09:43:52 +0200

    Added all the basic TS

---
+++ src/utils.ts
@@ -0,0 +1,53 @@
+const reviewTotalDisplay = document.querySelector("#reviews");
+const returningUserDisplay = document.querySelector("#returning-user");
+const userNameDisplay = document.querySelector("#user");
+import { LoyaltyUser } from "./enums";
+import { Review } from "./interfaces";
+
+export function showReviewTotal(
+  value: number,
+  reviewer: string,
+  isLoyalty: LoyaltyUser
+) {
+  const iconDisplay = LoyaltyUser.GOLD_USER ? "⭐" : "";
+  reviewTotalDisplay.innerHTML =
+    value.toString() +
+    " " +
+    "Review" +
+    makeMultiple(value) +
+    "| last reviewed by " +
+    reviewer +
+    " " +
+    iconDisplay;
+}
+
+export function populateUser(isReturning: boolean, userName: string) {
+  if (isReturning == true) {
+    returningUserDisplay.innerHTML = "back";
+  }
+  userNameDisplay.innerHTML = userName;
+}
+
+export function showDetails(
+  authorityStatus: boolean | Permissions,
+  element: HTMLDivElement,
+  price: number
+) {
+  if (authorityStatus) {
+    const priceDisplay = document.createElement("div");
+    priceDisplay.innerHTML = price.toString() + "/night";
+    element.appendChild(priceDisplay);
+  }
+}
+
+export function makeMultiple(value: number): string {
+  if (value > 1) {
+    return "s";
+  } else return "";
+}
+
+// Broken code
+export function getTopTwoReviews(reviews: Review[]): Review[] {
+  const sortedReviews = reviews.sort((a, b) => b.stars - a.stars);
+  return sortedReviews.slice(0, 2);
+}
